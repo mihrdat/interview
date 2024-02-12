14544f950c68206f4fb6a8e1da9788f9fb5ff6ab
Mehrdad Mahmoodpoor <mahmoodpoor.mihrdat@gmail.com>
2024-02-12 16:29:12 +0330

    fix: Change default value of is_active field

--- users/models.py
+++ users/models.py
@@ -34,7 +34,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(unique=True)
     is_staff = models.BooleanField(default=False)
     date_joined = models.DateTimeField(auto_now_add=True)
-    is_active = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
 
     EMAIL_FIELD = "email"
     USERNAME_FIELD = "email"
