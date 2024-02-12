1a650ea174f4f3b9e663edc6854367234e70466c
Mehrdad Mahmoodpoor <mahmoodpoor.mihrdat@gmail.com>
2024-02-12 21:53:46 +0330

    feat: Create Seller model

--- store/models.py
+++ store/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+User = get_user_model()
+
+
+class BaseModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class Seller(BaseModel):
+    first_name = models.CharField(max_length=55, blank=True, null=True)
+    last_name = models.CharField(max_length=55, blank=True, null=True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
