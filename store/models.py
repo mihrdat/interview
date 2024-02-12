4d5546614c5288aace764863c845c58587b431e7
Mehrdad Mahmoodpoor <mahmoodpoor.mihrdat@gmail.com>
2024-02-12 22:49:45 +0330

    feat: Create CreditTransactionLog model

--- store/models.py
+++ store/models.py
@@ -4,20 +4,25 @@ from django.contrib.auth import get_user_model
 User = get_user_model()
 
 
-class BaseModel(models.Model):
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-
-    class Meta:
-        abstract = True
-
-
-class Seller(BaseModel):
+class Seller(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     first_name = models.CharField(max_length=55, blank=True, null=True)
     last_name = models.CharField(max_length=55, blank=True, null=True)
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
 
 class Credit(models.Model):
     balance = models.DecimalField(max_digits=10, decimal_places=2)
     seller = models.OneToOneField(Seller, on_delete=models.CASCADE)
+
+
+class CreditTransactionLog(models.Model):
+    TYPE_CHOICES = [
+        ("DEPOSIT", "Deposit"),
+        ("WITHDRAWAL", "Withdrawal"),
+    ]
+    credit = models.ForeignKey(Credit, on_delete=models.CASCADE)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    type = models.CharField(max_length=10, choices=TYPE_CHOICES)
+    created_at = models.DateTimeField(auto_now_add=True)
