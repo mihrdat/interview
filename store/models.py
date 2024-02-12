2d71129ff516b3175df7ee8132663c33e20ea2d4
Mehrdad Mahmoodpoor <mahmoodpoor.mihrdat@gmail.com>
2024-02-12 22:01:39 +0330

    feat: Create Credit model

--- store/models.py
+++ store/models.py
@@ -16,3 +16,8 @@ class Seller(BaseModel):
     first_name = models.CharField(max_length=55, blank=True, null=True)
     last_name = models.CharField(max_length=55, blank=True, null=True)
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+
+class Credit(models.Model):
+    balance = models.DecimalField(max_digits=10, decimal_places=2)
+    seller = models.OneToOneField(Seller, on_delete=models.CASCADE)
