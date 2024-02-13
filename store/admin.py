f259b8dd7fa3ac2e46b0473b967db1d5a74a6a63
Mehrdad Mahmoodpoor <mahmoodpoor.mihrdat@gmail.com>
2024-02-13 10:27:34 +0330

    feat: Create approved selected action in admin panel

--- store/admin.py
+++ store/admin.py
@@ -1,3 +1,34 @@
+from django.db import transaction
 from django.contrib import admin
+from .models import DepositRequest, CreditTransactionLog
 
-# Register your models here.
+
+class DepositRequestAdmin(admin.ModelAdmin):
+    list_display = ["credit", "amount", "status", "created_at", "updated_at"]
+    actions = ["approve_selected"]
+    readonly_fields = ["credit", "amount", "created_at", "updated_at"]
+
+    def get_readonly_fields(self, request, obj=None):
+        if obj and obj.status == DepositRequest.STATUS_APPROVED:
+            return self.readonly_fields + ["status"]
+        return self.readonly_fields
+
+    @transaction.atomic
+    def approve_selected(self, request, queryset):
+        for deposit_request in queryset:
+            if deposit_request.status == DepositRequest.STATUS_PENDING:
+                deposit_request.status = DepositRequest.STATUS_APPROVED
+                deposit_request.save(update_fields=["status"])
+
+                credit = deposit_request.credit
+                credit.balance += deposit_request.amount
+                credit.save(update_fields=["balance"])
+
+                CreditTransactionLog.objects.create(
+                    credit=credit,
+                    amount=deposit_request.amount,
+                    type=CreditTransactionLog.TYPE_DEPOSIT,
+                )
+
+
+admin.site.register(DepositRequest, DepositRequestAdmin)
